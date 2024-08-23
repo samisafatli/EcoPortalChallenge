0428d07ed4d8437358b993ad1066be88064cf135
Sami Safatli <sami.safatli@yduqs.com.br>
2024-08-23 08:28:35 -0300

    Rename redux from example to movie

--- coolmovies-frontend/redux/store.ts
+++ coolmovies-frontend/redux/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { exampleReducer, exampleEpics } from './slices';
+import { moviesReducer, movieEpics } from './slices';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { CreateStoreOptions } from './types';
 
-const rootEpic = combineEpics(exampleEpics);
+const rootEpic = combineEpics(movieEpics);
 
 export const createStore = ({ epicDependencies }: CreateStoreOptions) => {
   const epicMiddleware = createEpicMiddleware({
@@ -15,7 +15,7 @@ export const createStore = ({ epicDependencies }: CreateStoreOptions) => {
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(epicMiddleware),
     reducer: {
-      example: exampleReducer,
+      movies: moviesReducer,
     },
   });
 
