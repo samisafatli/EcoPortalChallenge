d8d668f6da40dc271211f6cbde7619fd78840583
Sami Safatli <sami.safatli@yduqs.com.br>
2024-08-29 16:58:05 -0300

    Add new /reviews endpoint

--- coolmovies-frontend/pages/index.tsx
+++ coolmovies-frontend/pages/index.tsx
@@ -1,21 +1,14 @@
-import { css } from '@emotion/react';
-import { Paper, Typography } from '@mui/material';
 import type { NextPage } from 'next';
-import MovieList from '../components/MovieList/MovieList';
-import styles from './index.styles'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const Home: NextPage = () => {
-  return (
-    <div css={styles.root}>
-      <Paper elevation={3} css={styles.navBar}>
-        <Typography>{'EcoPortal'}</Typography>
-      </Paper>
+const HomePage: NextPage = () => {
+  const router = useRouter();
 
-      <div css={styles.body}>
-        <MovieList />
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    router.push('/reviews')
+  }, [router])
+
 };
 
-export default Home;
+export default HomePage;
---
+++ coolmovies-frontend/pages/reviews.tsx
@@ -0,0 +1,21 @@
+import { Paper, Typography } from '@mui/material'
+import { NextPage } from 'next'
+import React from 'react'
+import MovieList from '../components/MovieList/MovieList'
+import styles from './index.styles'
+
+const ReviewsPage: NextPage = () => {
+    return (
+        <div css={styles.root}>
+            <Paper elevation={3} css={styles.navBar}>
+                <Typography>{'EcoPortal Reviews'}</Typography>
+            </Paper>
+
+            <div css={styles.body}>
+                <MovieList />
+            </div>
+        </div>
+    )
+}
+
+export default ReviewsPage
